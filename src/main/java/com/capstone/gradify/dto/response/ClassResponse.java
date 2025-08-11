bafafbaa3cb278123b5d9f773d6eb94f30a4b684
Van Harvey Coca <vanharvey.coca@cit.edu>
2025-08-11 17:58:21 +0800

    ADDED: attribute UpdateAt on ClassResponse

--- src/main/java/com/capstone/gradify/dto/response/ClassResponse.java
+++ src/main/java/com/capstone/gradify/dto/response/ClassResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
@@ -18,5 +19,6 @@ public class ClassResponse {
     private String section;
     private String schedule;
     private String room;
+    private Date updatedAt;
     private List<StudentResponse> students;
 }
